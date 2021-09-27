d59e7b451b3e1b6d8b4df8de6e839efb790012ea
Geoffrey C. Hutchinson <geoffreyhutchinson018@gmail.com>
2021-09-27 21:37:46 +0000

    fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ldhwy^6(+((xf99%aiknefxxi+@ka03+(88rwqk4tnpdmf9_-b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gch21-django-todo-app.herokuapp.com']
 
 
 # Application definition
